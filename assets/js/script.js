488ef4b59363822fbb09e9bf038c78f789da0a6f
Yousouf Mustafa Muhammed <yousoufmostafamuslim@gmail.com>
2021-01-06 21:13:39 +0200

    responsive navbar css 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
+var navbarMenu = document.querySelector(".navbar__menu");
 
+document.querySelector(".mobile__toggle").addEventListener('click', function () {
+    navbarMenu.classList.toggle("active")
+});
