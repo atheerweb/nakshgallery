6e1927954e689956f7baf81b4ece149a7195c25b
Yousouf Mustafa Muhammed <yousoufmostafamuslim@gmail.com>
2021-01-06 21:36:45 +0200

    navbar 2 animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var navbarMenu = document.querySelector(".navbar__menu");
+var navbarToggle = document.querySelector("#mobile-menu")
 
 document.querySelector(".mobile__toggle").addEventListener('click', function () {
     navbarMenu.classList.toggle("active")
+    navbarToggle.classList.toggle("isactive")
 });
