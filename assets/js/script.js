7a2cc4bb812bc987fa82fdda68d0fe7b3134040e
Yousouf Mustafa Muhammed <yousoufmostafamuslim@gmail.com>
2021-01-12 16:38:37 +0200

    removing row gaps

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
+//responsive menu 
+
 var navbarMenu = document.querySelector(".navbar__menu");
 var navbarToggle = document.querySelector("#mobile-menu")
+var navLogo = document.querySelector("#navbar__logo")
+
 
 document.querySelector(".mobile__toggle").addEventListener('click', function () {
     navbarMenu.classList.toggle("active")
     navbarToggle.classList.toggle("isactive")
 });
-
