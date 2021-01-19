cf7f9bcb692d0f32942636dba770a1279415eb5a
Yousouf Mustafa Muhammed <yousoufmostafamuslim@gmail.com>
2021-01-19 17:06:11 +0200

    js scrolling border effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,35 @@ document.querySelector(".mobile__toggle").addEventListener('click',  () => {
 });
 
 
+// Show active menu when scrolling
+
+const highlightMenu = () => {
+    const bottomborder = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#aboutus-page')
+    const serviceMenu = document.querySelector('#services-page')
+    let scrollP = window.scrollY
+    console.log(scrollP)
+
+ //adding highlight class
+ if(window.innerWidth > 960 && scrollP < 168){
+     homeMenu.classList.add("highlight")
+     aboutMenu.classList.remove("highlight")
+     serviceMenu.classList.remove("highlight")
+     return
+ }else if (window.innerWidth > 960 && scrollP < 991){
+    aboutMenu.classList.add("highlight")
+    homeMenu.classList.remove("highlight")
+    serviceMenu.classList.remove("highlight")
+    return
+ }else if (window.innerWidth > 960 && scrollP < 2048){
+    serviceMenu.classList.add("highlight")
+    aboutMenu.classList.remove("highlight")
+   return
+ }
+ if((bottomborder && window.innerWidth < 960 && scrollP < 168) || bottomborder){
+    bottomborder.classList.remove("highlight")
+}
+}
+window.addEventListener('scroll' , highlightMenu)
+window.addEventListener('click' , highlightMenu)
